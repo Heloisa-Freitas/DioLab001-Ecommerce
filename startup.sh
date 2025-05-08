4d028b12a855fb4362de6f1636b53f18a7d8796e
Heloisa-Freitas <heloisacarvalho@outlook.com>
2025-05-08 16:51:51 -0400

    novo venv

--- startup.sh
+++ startup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-echo "Iniciando app..."
+echo "Iniciando aplicação..."
 
+# Ativa o ambiente virtual criado automaticamente pelo Azure
+source antenv/bin/activate
+
+# Executa o Streamlit
 python3 -m streamlit run main.py --server.port=8000 --server.enableCORS=false
