36886962ead98f9d8d8666e6276221973c41f07d
Heloisa-Freitas <heloisacarvalho@outlook.com>
2025-05-08 14:19:14 -0400

    Corrigir ativação do venv no startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+# Ativar o ambiente virtual (se criado com o GitHub Actions, pode estar em /antenv ou similar)
+if [ -d "venv" ]; then
+    source venv/bin/activate
+elif [ -d "antenv" ]; then
+    source antenv/bin/activate
+fi
+
+# Executar o app Streamlit
 streamlit run main.py --server.port=8000 --server.enableCORS=false
