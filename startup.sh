5a340e49bfef8a556cfc5af05bb42ee828c23e0b
Heloisa-Freitas <heloisacarvalho@outlook.com>
2025-05-08 15:44:39 -0400

    Corrigir ambiente para compatibilidade com Azure Linux

--- startup.sh
+++ startup.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
+echo "Iniciando app..."
 
-echo "Iniciando startup.sh..."
-
-# Ativa o ambiente virtual criado automaticamente pelo Oryx
-source /home/site/wwwroot/antenv/bin/activate
-
-# Executa o app Streamlit
 python3 -m streamlit run main.py --server.port=8000 --server.enableCORS=false
