600453194ebb01b415fc707426f010c9349889db
Heloisa-Freitas <heloisacarvalho@outlook.com>
2025-05-08 19:09:57 -0400

    novo projeto

--- startup.sh
+++ startup.sh
@@ -1,8 +1,2 @@
-#!/bin/bash
-echo "Iniciando aplicação..."
-
-# Ativa o ambiente virtual criado automaticamente pelo Azure
-source antenv/bin/activate
-
-# Executa o Streamlit
-python3 -m streamlit run main.py --server.port=8000 --server.enableCORS=false
+- name: Garantir permissão de execução
+  run: chmod +x startup.sh
